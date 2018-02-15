1d51e15467e30ba8425e01d98297c5fb7925774d
galakerman <akermang@gmail.com>
2018-02-15 13:19:35 +0200

    github oser auth login option added

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var uiConfig = {
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID
     // firebase.auth.PhoneAuthProvider.PROVIDER_ID
   ],
